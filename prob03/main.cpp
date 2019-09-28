8499917f6de90c0b4fda8e7c8faab9bb0b970891
minhee71 <54484119+minhee71@users.noreply.github.com>
2019-09-28 01:51:36 -0700

    Update main.cpp

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -2,21 +2,21 @@
 // Sales prediction
 
 #include <iostream>
+
 int main ()
-
 {
-// Defining the variables.
-long long predicted_sales, total_increase;
-const int PRIOR_SALES_TOTAL = 2103419277;
-const float PERCENT_INCREASE = .18;
+  // Defining the variables.
+  long long predicted_sales, total_increase;
+  const int PRIOR_SALES_TOTAL = 2103419277;
+  const float PERCENT_INCREASE = .18;
 
-// Calculating the current year-to-date sales.
-total_increase = PRIOR_SALES_TOTAL * PERCENT_INCREASE;
-predicted_sales = total_increase + PRIOR_SALES_TOTAL;
+  // Calculating the current year-to-date sales.
+  total_increase = PRIOR_SALES_TOTAL * PERCENT_INCREASE;
+  predicted_sales = total_increase + PRIOR_SALES_TOTAL;
 
-// Output of the results.
-std::cout << "Last year\'s sales were $" << PRIOR_SALES_TOTAL << std::endl;
-std::cout << "This years\'s sales prediction: $" << predicted_sales << std::endl << std::endl;
+  // Output of the results.
+  std::cout << "Last year\'s sales were $" << PRIOR_SALES_TOTAL << std::endl;
+  std::cout << "This years\'s sales prediction: $" << predicted_sales << std::endl << std::endl;
 
-return 0;
+  return 0;
 }
